1b564adcc8086543bcd4e3ee8f620292fac3fb02
Noah Dailey <nwd1654@rit.edu>
2026-09-10 14:34:36 -0400

    Revert "Continued attempting to put together a Mesh class for easier drawing."
    This reverts commit 076b11f7c32a12468a779af63679f1638f777897.

--- Mesh.h
+++ Mesh.h
@@ -2,32 +2,28 @@
 
 #include <d3d11.h>
 #include <wrl/client.h>
-#include <DirectXMath.h>
 
 class Mesh 
 {
 public:
-	// Construction and Deconstruction
-	Mesh(Vertex vertices[], unsigned int indices[]);
-	~Mesh();
-	
-	// Methods for creating the buffers
+	/// <summary>
+	/// Get access to the vertex buffer
+	/// </summary>
+	/// <returns>A Comptr to the vertex buffer</returns>
 	Microsoft::WRL::ComPtr<ID3D11Buffer> GetVertexBuffer();
+
+	/// <summary>
+	/// Get access to the index buffer
+	/// </summary>
+	/// <returns>A ComPtr to the Index Buffer</returns>
 	Microsoft::WRL::ComPtr<ID3D11Buffer> GetIndexBuffer();
-
-	// Methods for recieving the count of the buffers
-	int GetIndexCount();
-	int GetVertexCount();
-
-	// Method for drawing to the screen
-	void Draw();
 
 private:
 	// Buffers to hold actual geometry data
 	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
 	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
 
-	// Ints to track the number of indices and vertices
-	int totalIndices = 0;
-	int totalVertices = 0;
+	// Ints to track the number of indeces and vertices
+	int totalIndeces;
+	int totalVertices;
 };
